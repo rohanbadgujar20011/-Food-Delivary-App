d4f1710cc5b92719a5425e1427450a9cc63b7160
rohanbadgujar20011 <rohanbadgujar2001@gmail.com>
2025-09-17 09:03:36 +0530

    Added Auth service

---
+++ server/auth-service/src/main/java/com/fooddelivery/auth/configuration/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.auth.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+}
